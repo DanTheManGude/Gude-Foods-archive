36dd3fab198aa86e62d35fc9076477a096cf5bd8
DanTheManGude <djg4755@rit.edu>
2019-03-10 23:32:19 -0400

    View the recipes on Home

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -7,6 +7,7 @@ export class Recipe extends Component {
         super(props);
 
         this.handleEdit = this.handleEdit.bind(this);
+        this.state = {expand: false};
     }
 
     handleEdit(event) {
@@ -18,12 +19,48 @@ export class Recipe extends Component {
     }
 
     render() {
+        var expandText;
+        var expandStyle;
+        if (this.state.expand){
+            expandText = <i className="fas fa-caret-down"></i>;
+            expandStyle = {display: ''};
+        }
+        else {
+            expandText = <i className="fas fa-caret-right"></i>;
+            expandStyle = {display: 'none'};
+        }
+
         return (
             <div>
-                <Link to="/Gude-Foods/edit" onClick={this.handleEdit}><strong>{this.props.recipe.name}</strong>—&nbsp;</Link>
-                {this.props.recipe.about.purpose} |&nbsp;
-                {this.props.recipe.about.time} |&nbsp;
-                {this.props.recipe.about.makes}
+                <span>
+                    <button className="btn-link btn" onClick={() => {
+                        this.setState({expand: !this.state.expand})
+                    }}>{expandText}</button>
+                    <strong>{this.props.recipe.name}</strong>—&nbsp;
+                    {this.props.recipe.about.purpose} |&nbsp;
+                    {this.props.recipe.about.time} |&nbsp;
+                    {this.props.recipe.about.makes} |&nbsp;
+                    <Link to="/Gude-Foods/edit" onClick={this.handleEdit}><i className="fa fa-cogs" aria-hidden="true"></i></Link>
+                </span>
+                <ul style={expandStyle}>
+                    <li>Notes: {this.props.recipe.notes}</li>
+                    <li>Ingredients:</li>
+                    <ul>
+                        {this.props.recipe.ingredients.map(ingredient =>
+                            <li>
+                                {ingredient.quantity} of {ingredient.what}
+                            </li>
+                        )}
+                    </ul>
+                    <li>Procedure:</li>
+                    <ol>
+                        {this.props.recipe.procedure.map(step =>
+                            <li>
+                                {step}
+                            </li>
+                        )}
+                    </ol>
+                </ul>
             </div>
         );
     }
