1cc11591c90cb35246759004e0c4120c1c44ebcb
Daniel Gude <dgude31@gmail.com>
2020-02-09 15:09:09 -0500

    Add prettier

--- src/application.test.js
+++ src/application.test.js
@@ -1,25 +1,23 @@
-
-const { expect } = require('chai');
-const Application = require('./application');
+const { expect } = require("chai");
+const Application = require("./application");
 
 var app = new Application();
 
-describe('ping Route', () => {
-  it('ping pong', () => {
-    expect(Application.ping()).to.equal('pong');
+describe("ping Route", () => {
+  it("ping pong", () => {
+    expect(Application.ping()).to.equal("pong");
   });
 });
 
-describe('revision Route', () => {
+describe("revision Route", () => {
   var result = Application.revision();
-  it('Has proper keys', () => {
-    expect(result).to.have.all.keys(['version', 'hash']);
+  it("Has proper keys", () => {
+    expect(result).to.have.all.keys(["version", "hash"]);
   });
-  it('Find version', () => {
-    expect(result.version).to.not.contain('version');
+  it("Find version", () => {
+    expect(result.version).to.not.contain("version");
   });
-  it('Find hash', () => {
-    expect(result.hash).to.not.contain('hash');
+  it("Find hash", () => {
+    expect(result.hash).to.not.contain("hash");
   });
 });
-
