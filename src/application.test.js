8b1d1e05124f81d9bd2e37da7201f6c6efe14cc8
Daniel Gude <dgude31@gmail.com>
2020-02-09 14:31:24 -0500

    Starter boilerplate

---
+++ src/application.test.js
@@ -0,0 +1,25 @@
+
+const { expect } = require('chai');
+const Application = require('./application');
+
+var app = new Application();
+
+describe('ping Route', () => {
+  it('ping pong', () => {
+    expect(Application.ping()).to.equal('pong');
+  });
+});
+
+describe('revision Route', () => {
+  var result = Application.revision();
+  it('Has proper keys', () => {
+    expect(result).to.have.all.keys(['version', 'hash']);
+  });
+  it('Find version', () => {
+    expect(result.version).to.not.contain('version');
+  });
+  it('Find hash', () => {
+    expect(result.hash).to.not.contain('hash');
+  });
+});
+
---
+++ src/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const Application = require('./application');
+
+var app = new Application();
+const server = express();
+
+const BUILDPATH = '../frontend/build';
+const PORT = process.env.PORT || 3030;
+
+const logger = (req, res, next) => {
+  app.logReq(req);
+  next();
+};
+
+server.use(logger);
+server.use(cors());
+server.use(bodyParser.json());
+server.use(express.static(path.join(__dirname, BUILDPATH)));
+
+server.get('/ping', (req, res) => {
+  res.send(Application.ping());
+});
+
+server.get('/api/revision', (req, res) => {
+  res.send(Application.revision());
+});
+
+server.get('/api/logs', (req, res, next) => {
+    res.send(app.getLogs());
+  });
+
+server.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, BUILDPATH, 'index.html'));
+});
+
+server.use((err, req, res, next) => {
+  var statusCode = err.status || 500;
+  res.end(res.writeHead(statusCode, err.message));
+});
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
